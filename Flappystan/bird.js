30d3cfd92f4cd7ed4b6ac41344804371d4843240
She Queen <shequeen@Shes-MacBook-Pro.local>
2018-12-12 19:49:09 -0800

    new

--- Flappystan/bird.js
+++ Flappystan/bird.js
@@ -2,15 +2,21 @@ function Bird(){
   this.y = height/2;
   this.x =64;
 //bird falling down
-this.gravity = 1;
+this.gravity = 0.6;
+this.lift = -15;
 this.velocity = 0;
 // bird looking
   this.show = function(){
     fill(255);
     ellipse(this.x, this.y, 32, 32);
   }
+  this.up = function() {
+    this.velocity += this.lift;
+  }
+
   this.update = function(){
     this.velocity += this.gravity;
+    this.velocity += 0.9;
     this.y += this.velocity;
 //bird position
     if (this.y > height) {
